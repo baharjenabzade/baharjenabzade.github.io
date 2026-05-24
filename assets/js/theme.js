7298a7785e6caf0eaa351f5ef7970d6c688b240c
Bahar Jenabzadeh <baharjbz@gmail.com>
2026-05-25 01:09:19 +0330

    Dark Mode

--- assets/js/theme.js
+++ assets/js/theme.js
@@ -1,5 +1,26 @@
-const toggle = document.getElementById("theme-toggle");
+document.addEventListener("DOMContentLoaded", () => {
 
-toggle.addEventListener("click", () => {
-    document.body.classList.toggle("dark");
+    const toggle = document.getElementById("theme-toggle");
+
+    if (!toggle) return;
+
+    // load saved theme
+    const savedTheme = localStorage.getItem("theme");
+
+    if (savedTheme === "dark") {
+        document.body.classList.add("dark");
+    }
+
+    toggle.addEventListener("click", () => {
+
+        document.body.classList.toggle("dark");
+
+        if (document.body.classList.contains("dark")) {
+            localStorage.setItem("theme", "dark");
+        } else {
+            localStorage.setItem("theme", "light");
+        }
+
+    });
+
 });
